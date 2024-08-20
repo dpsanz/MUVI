b9b34d41830225f0271ce2d9f4b43783b9d470bc
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-20 12:13:03 -0300

    update: barra de pesquisa que funciona

--- muvi-react/src/Pages/Filmes.jsx
+++ muvi-react/src/Pages/Filmes.jsx
@@ -1,22 +1,36 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 // eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Filmes() {
-    const [filmesPopulares, setFilmesPopulares, ] = useState([]);
-    const [filmesEmCartaz, setFilmesEmCartaz, ] = useState([]);
-    const [proximosLancamentos, setProximosLancamentos] = useState([])
+    const [filmesPopulares, setFilmesPopulares] = useState([]);
+    const [filmesEmCartaz, setFilmesEmCartaz] = useState([]);
+    const [proximosLancamentos, setProximosLancamentos] = useState([]);
     const [page] = useState(1);
+
+    const [searchInput, setSearchInput] = useState('');
+    const [filteredMovies, setFilteredMovies] = useState([]);
 
     const apiKey = 'api_key=7c572a9f5b3ba776080330d23bb76e1e';
     const urlBase = 'https://api.themoviedb.org/3/movie/';
     const urlImg = 'https://image.tmdb.org/t/p/w342/';
 
+    useEffect(() => {
+        fetchPopularMovies(page);
+        fetchNowPlayingMovies(page);
+        fetchProximosLancamentos(page);
+    }, [page]);
+
+    useEffect(() => {
+        filterMovies();
+    }, [filmesPopulares, filmesEmCartaz, proximosLancamentos, searchInput]);
+
     const fetchPopularMovies = (pageNum) => {
         fetch(`${urlBase}popular?${apiKey}&page=${pageNum}`)
             .then(response => response.json())
             .then(response => {
-                setFilmesPopulares(prevFilmes => [...prevFilmes, ...response.results]);
+                setFilmesPopulares(response.results);
             })
             .catch(erro => console.log(erro));
     };
@@ -25,7 +39,7 @@ function Filmes() {
         fetch(`${urlBase}now_playing?${apiKey}&page=${pageNum}`)
             .then(response => response.json())
             .then(response => {
-                setFilmesEmCartaz(prevFilmes => [...prevFilmes, ...response.results]);
+                setFilmesEmCartaz(response.results);
             })
             .catch(erro => console.log(erro));
     };
@@ -34,66 +48,65 @@ function Filmes() {
         fetch(`${urlBase}upcoming?${apiKey}&page=${pageNum}`)
             .then(response => response.json())
             .then(response => {
-                setProximosLancamentos(prevFilmes => [...prevFilmes, ...response.results]);
+                setProximosLancamentos(response.results);
             })
             .catch(erro => console.log(erro));
     };
 
-    useEffect(() => {
-        fetchPopularMovies(page);
-        fetchNowPlayingMovies(page);
-        fetchProximosLancamentos(page);
-    }, [page]);
+    const filterMovies = () => {
+        if (searchInput.trim() === '') {
+            setFilteredMovies([]);
+            return;
+        }
+
+        const lowerCaseInput = searchInput.toLowerCase();
+
+        const todosFilmes = [
+            ...filmesPopulares,
+            ...filmesEmCartaz,
+            ...proximosLancamentos
+        ];
+
+        const SoloFilmes = Array.from(new Set(todosFilmes.map(filme => filme.id)))
+            .map(id => todosFilmes.find(filme => filme.id === id));
+
+        const filtered = SoloFilmes.filter(filme => {
+            const title = filme.title.toLowerCase();
+            return title.startsWith(lowerCaseInput) || title.includes(lowerCaseInput);
+        });
+
+        setFilteredMovies(filtered);
+    };
+
+    const handleInputChange = (e) => {
+        setSearchInput(e.target.value);
+    };
 
     return (
         <>
             <main className="bg-black">
-                <div className='flex flex-col items-center'>
-                    <h1 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-teal-600 to-teal-100 mb-8">FILMES POPULARES</h1>
-                    
-                    <div className='w-full flex justify-center'>
-                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-3 mx-10">
-                            {filmesPopulares.slice(0,15).map(filme => (
-                                <div className="card-filme flex flex-col items-center" key={filme.id}>
-                                    <Link to={`${filme.id}`} className="text-teal-800 mt-1 hover:tracking-widest transition-all font-bold">
-                                        <img className="self-center opacity-70 hover:opacity-100 transition-all" src={`${urlImg}${filme.poster_path}`} alt={filme.title} />
-                                    </Link>
-                                    <h1 className="text-white text-center mt-2 font-bold text-2xl">{filme.title}</h1>
-                                    <Link to={`${filme.id}`} className="text-teal-800 mt-1 hover:tracking-widest transition-all font-bold">Ver Mais</Link>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-
-                <div className='flex flex-col items-center'>
-                    <h1 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-teal-600 to-teal-100 my-8">
-                        FILMES EM CARTAZ
-                    </h1>
-                    
-                    <div className='w-full flex justify-center'>
-                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-3 mx-10">
-                            {filmesEmCartaz.slice(0,8).map(filme => (
-                                <div className="card-filme flex flex-col items-center" key={filme.id}>
-                                    <Link to={`${filme.id}`} className="text-teal-800 mt-1 hover:tracking-widest transition-all font-bold">
-                                        <img className="self-center opacity-70 hover:opacity-100 transition-all" src={`${urlImg}${filme.poster_path}`} alt={filme.title} />
-                                    </Link> 
-                                    <h1 className="text-white text-center mt-2 font-bold text-2xl">{filme.title}</h1>
-                                    <Link to={`${filme.id}`} className="text-teal-800 mt-1 hover:tracking-widest transition-all font-bold">Ver Mais</Link>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-
-                <div className='flex flex-col items-center mt-8'>
+                <div className="flex flex-col items-center">
                     <h1 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-teal-600 to-teal-100 mb-8">
-                        PRÓXIMOS LANÇAMENTOS
+                        FILMES
                     </h1>
                     
-                    <div className='w-full flex justify-center mb-5'>
+                    <input
+                        type="text"
+                        value={searchInput}
+                        onChange={handleInputChange}
+                        placeholder="Pesquisar filmes..."
+                        className="mb-6 p-2 rounded border border-gray-300"
+                    />
+
+                    {searchInput && (
+                        <h1 className="text-xl text-white mb-6">
+                            Resultados para: {searchInput}
+                        </h1>
+                    )}
+
+                    <div className="w-full flex justify-center">
                         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-3 mx-10">
-                            {proximosLancamentos.slice(0,15).map(filme => (
+                            {(filteredMovies.length > 0 ? filteredMovies : filmesPopulares.slice(0, 15)).map(filme => (
                                 <div className="card-filme flex flex-col items-center" key={filme.id}>
                                     <Link to={`${filme.id}`} className="text-teal-800 mt-1 hover:tracking-widest transition-all font-bold">
                                         <img className="self-center opacity-70 hover:opacity-100 transition-all" src={`${urlImg}${filme.poster_path}`} alt={filme.title} />
--- muvi-react/src/Pages/News.jsx
+++ muvi-react/src/Pages/News.jsx
@@ -1,29 +1,51 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useState } from 'react';
 import noticiasData from '../../noticias.json';
 
 const Noticias = () => {
+    const [searchInput, setSearchInput] = useState('');
+    const [filteredArticles, setFilteredArticles] = useState(noticiasData);
+
+    const handleInputChange = (e) => {
+        const searchTerm = e.target.value;
+        setSearchInput(searchTerm);
+
+        const filteredResults = noticiasData.filter((noticia) =>
+            noticia.titulo.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+
+        setFilteredArticles(filteredResults);
+    };
+
     return (
         <main className='bg-black'>
             <div className="flex flex-col items-center bg-black">
-            <h1 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-teal-600 to-teal-100 mb-8">
-                        NOTÍCIAS
+                <h1 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-teal-600 to-teal-100 mb-8">
+                    NOTÍCIAS
                 </h1>
 
+                <input
+                    type="text"
+                    value={searchInput}
+                    onChange={handleInputChange}
+                    placeholder="Pesquisar notícias..."
+                    className="mb-6 p-2 rounded border border-gray-300"
+                />
+
                 <div className="container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4 mb-10 px-10">
-                    {noticiasData.slice(0, 5).map((noticia, index) => (
+                    {filteredArticles.slice(0, 5).map((noticia, index) => (
                         <div key={noticia.id} className="p-4 shadow-md relative"
-                            style={{ 
-                                backgroundImage: `url(/public/${index + 1}.png)`, 
-                                backgroundSize: 'cover', 
-                                backgroundPosition: 'center', 
-                                color: 'white', 
-                                borderRadius: '8px', 
-                                marginBottom: '1rem', 
+                            style={{
+                                backgroundImage: `url(/public/${index + 1}.png)`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                color: 'white',
+                                borderRadius: '8px',
+                                marginBottom: '1rem',
                                 height: '400px',
                             }}
                         >
-                            <div className="absolute inset-0 bg-black opacity-60 rounded-md "></div>
+                            <div className="absolute inset-0 bg-black opacity-60 rounded-md"></div>
                             <div className="flex flex-col items-center justify-center h-full relative z-10">
                                 <h2 className="text-lg font-bold mb-2">{noticia.titulo}</h2>
                                 <p className="text-gray-400">{noticia.descricao}</p>
@@ -35,9 +57,9 @@ const Noticias = () => {
             </div>
         </main>
     );
-}
+};
 
-// Função auxiliar para formatar a data (opcional)
+// Função auxiliar para formatar a data
 function formatarData(dataString) {
     const data = new Date(dataString);
     return data.toLocaleDateString('pt-BR', { year: 'numeric', month: 'long', day: 'numeric' });
